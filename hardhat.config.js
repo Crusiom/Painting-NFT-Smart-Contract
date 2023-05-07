29a94da9d8420c1aad886dd0d8d44a7fc4a1e84c
JerryWu1998 <116984762+JerryWu1998@users.noreply.github.com>
2023-05-07 10:53:44 -0400

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,9 +3,9 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   solidity: "0.8.4",
   paths: {
-    artifacts: "./src/contract/artifacts",
-    sources: "./src/contract/contracts",
-    cache: "./src/contract/cache",
-    tests: "./src/contract/test"
-  }
+    artifacts: "./src/backend/artifacts",
+    sources: "./src/backend/contracts",
+    cache: "./src/backend/cache",
+    tests: "./src/backend/test"
+  },
 };
